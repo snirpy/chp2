aa59dbc1670a09196e0f2296f5463b1d0c883b18
snirpy <76662720+snirpy@users.noreply.github.com>
2024-10-14 19:52:52 +0200

    Update Ex1_moyenne_appreciation.py

--- Ex1_moyenne_appreciation.py
+++ Ex1_moyenne_appreciation.py
@@ -1,18 +1,18 @@
-moyenne = 0
-while moyenne == 0:
+continuer = True
+while continuer:
     moyenne = input("Entrer votre moyenne: ")
     try:
         moyenne = int(moyenne)
     except:
         print("Vous n'avez pas saisi une valeur numérique")
-        moyenne = 0
+       
     else:
         if moyenne < 0 :
             print("La moyenne ne peut être négative!")
-            moyenne = 0
+            
         elif moyenne > 20:
             print("La moyenne ne peut être supériure à 20!")
-            moyenne = 0
+            
         else:
             if moyenne >= 18:
                 print(f"Excellent! Vous avez eu {moyenne} de moyenne")
@@ -24,6 +24,6 @@ while moyenne == 0:
                 print(f"Insuffisant! Vous avez eu {moyenne} de moyenne")
             elif moyenne < 5:
                 print(f"Catastrophique! Vous avez eu {moyenne} de moyenne")
+            continuer = False
             
-
 
